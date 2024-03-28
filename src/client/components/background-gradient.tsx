71c4fe8df3ed073148ddb1fedb2aad2d3cbb7504
malezjaa <66274761+malezjaa@users.noreply.github.com>
2024-03-28 21:58:20 +0100

    chore: cleanup

--- src/client/components/background-gradient.tsx
+++ src/client/components/background-gradient.tsx
@@ -3,8 +3,6 @@ import React, { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 export const BackgroundGradientAnimation = ({
-  gradientBackgroundStart = "rgb(108, 0, 162)",
-  gradientBackgroundEnd = "rgb(0, 17, 82)",
   firstColor = "18, 113, 255",
   secondColor = "221, 74, 255",
   thirdColor = "100, 220, 255",
@@ -18,8 +16,6 @@ export const BackgroundGradientAnimation = ({
   interactive = true,
   containerClassName,
 }: {
-  gradientBackgroundStart?: string;
-  gradientBackgroundEnd?: string;
   firstColor?: string;
   secondColor?: string;
   thirdColor?: string;
@@ -35,14 +31,6 @@ export const BackgroundGradientAnimation = ({
 }) => {
   const location = useLocation();
   useEffect(() => {
-    document.body.style.setProperty(
-      "--gradient-background-start",
-      gradientBackgroundStart
-    );
-    document.body.style.setProperty(
-      "--gradient-background-end",
-      gradientBackgroundEnd
-    );
     document.body.style.setProperty("--first-color", firstColor);
     document.body.style.setProperty("--second-color", secondColor);
     document.body.style.setProperty("--third-color", thirdColor);
@@ -90,33 +78,6 @@ export const BackgroundGradientAnimation = ({
           isSafari ? "blur-2xl" : "[filter:url(#blurMe)_blur(40px)]"
         )}
       >
-        {/*<div*/}
-        {/*  className={cn(*/}
-        {/*    `absolute [background:radial-gradient(circle_at_center,_var(--second-color)_0,_transparent_50%)_no-repeat]`,*/}
-        {/*    `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,*/}
-        {/*    `[transform-origin:calc(50%-400px)]`,*/}
-        {/*    `animate-second`,*/}
-        {/*    `opacity-100`*/}
-        {/*  )}*/}
-        {/*></div>*/}
-        {/*<div*/}
-        {/*  className={cn(*/}
-        {/*    `absolute [background:radial-gradient(circle_at_center,_var(--third-color)_0,_transparent_50%)_no-repeat]`,*/}
-        {/*    `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,*/}
-        {/*    `[transform-origin:calc(50%+400px)]`,*/}
-        {/*    `animate-third`,*/}
-        {/*    `opacity-100`*/}
-        {/*  )}*/}
-        {/*></div>*/}
-        {/*<div*/}
-        {/*  className={cn(*/}
-        {/*    `absolute [background:radial-gradient(circle_at_center,_var(--fifth-color)_0,_transparent_50%)_no-repeat]`,*/}
-        {/*    `[mix-blend-mode:var(--blending-value)] w-[var(--size)] h-[var(--size)] top-[calc(50%-var(--size)/2)] left-[calc(50%-var(--size)/2)]`,*/}
-        {/*    `[transform-origin:calc(50%-800px)_calc(50%+800px)]`,*/}
-        {/*    `animate-fifth`,*/}
-        {/*    `opacity-100`*/}
-        {/*  )}*/}
-        {/*></div>*/}
         <div
           className={cn(
             `fixed [background:radial-gradient(circle_at_center,_var(--second-color)_0,_transparent_50%)_no-repeat]`,
